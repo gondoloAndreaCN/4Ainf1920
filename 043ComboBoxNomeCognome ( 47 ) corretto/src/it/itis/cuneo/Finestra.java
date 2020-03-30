41c3e72a6b80d2100f376d329f32fcede67e8c9e
gondoloAndreaCN <57175185+gondoloAndreaCN@users.noreply.github.com>
2020-03-30 17:51:03 +0200

    Add files via upload

---
+++ 043ComboBoxNomeCognome ( 47 ) corretto/src/it/itis/cuneo/Finestra.java
@@ -0,0 +1,85 @@
+package it.itis.cuneo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Finestra extends JFrame implements ActionListener {
+
+    JLabel lnome;
+    JLabel lcognome;
+    JTextField tnome;
+    JTextField tcognome;
+    JButton bAggiungi;
+
+    JList listaDiNomi;
+    DefaultListModel listModel;
+
+
+    JComboBox comboBox;
+    DefaultComboBoxModel comboBoxModel;
+
+    public Finestra(){
+        setTitle("Lista nomi e cognomi");
+        setLocation(100, 200);
+        initComponent();
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void initComponent() {
+        setLayout(new FlowLayout());
+
+        lnome = new JLabel("nome");
+        this.add(lnome);
+
+        lcognome = new JLabel("cognome");
+        this.add(lcognome);
+
+        tnome = new JTextField(20);
+        this.add(tnome);
+
+        tcognome = new JTextField(20);
+        this.add(tcognome);
+
+        listaDiNomi = new JList();
+        this.add(listaDiNomi);
+
+        bAggiungi = new JButton("inserisci");
+        this.add(bAggiungi);
+        bAggiungi.addActionListener(this);
+
+        listModel = new DefaultListModel();
+        listaDiNomi = new JList(listModel);
+        JScrollPane jScrollPane = new JScrollPane(listaDiNomi);
+        this.add(jScrollPane);
+
+        comboBoxModel = new DefaultComboBoxModel();
+        JComboBox comboText = new JComboBox(comboBoxModel);
+        JScrollPane jScrollPane2 = new JScrollPane(comboText);
+        this.add(jScrollPane2);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource().equals(bAggiungi)){
+            if (tnome.getText().equals(" ") || tcognome.getText().equals("")){
+                JOptionPane.showMessageDialog(null, "Parametri non validi");
+            }else{
+                String stringa = tcognome.getText() + " " + tnome.getText();
+                listModel.addElement(stringa);
+                comboBoxModel.addElement(stringa);
+                tcognome.setText("");
+                tnome.setText("");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Finestra finestra = new Finestra();
+    }
+}
